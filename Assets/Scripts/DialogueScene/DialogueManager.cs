ff4643100be350759d6257193d937cd21a5805cb
lilasilb <silbermanlila@gmail.com>
2026-04-25 18:52:36 -0700

    Implement Dialogue sequence and display

--- Assets/Scripts/DialogueScene/DialogueManager.cs
+++ Assets/Scripts/DialogueScene/DialogueManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
+    public TMP_Text dialogueText;
+    
+    public Animator animator;
+
     private Queue<string> sentences;
 
     void Start()
@@ -13,5 +19,33 @@ public class DialogueManager : MonoBehaviour
     public void StartDialogue(Dialogue dialogue) 
     {
         Debug.Log("Starting conversation...");
+
+        animator.SetBool("isOpen", true);
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+    
+    void EndDialogue()
+    {        Debug.Log("End of conversation.");
+        dialogueText.text = "Meow";
+        animator.SetBool("isOpen", false);
     }
 }
