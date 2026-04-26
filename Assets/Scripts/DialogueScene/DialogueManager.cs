d2f7fbabe8ddbe32361e0c75428edd7b629f75dc
lilasilb <silbermanlila@gmail.com>
2026-04-25 19:17:29 -0700

    Add character enumerator

--- Assets/Scripts/DialogueScene/DialogueManager.cs
+++ Assets/Scripts/DialogueScene/DialogueManager.cs
@@ -40,7 +40,18 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
-        dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.05f);
+        }
     }
     
     void EndDialogue()
