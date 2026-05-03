1e21ba566ae5d8bdbfcfb3c6729cd461806de9ab
lilasilb <silbermanlila@gmail.com>
2026-05-03 12:04:14 -0700

    Autocomplete dialogue and press E to pick up

--- Assets/Scripts/DialogueScene/DialogueManager.cs
+++ Assets/Scripts/DialogueScene/DialogueManager.cs
@@ -2,65 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
     public TMP_Text dialogueText;
-    
     public Animator animator;
 
     private Queue<string> sentences;
+    private bool isTyping = false;
+    private string currentSentence = "";
 
     void Start()
     {
         sentences = new Queue<string>();
         Debug.Log("DialogueManager initialized.");
     }
-    public void StartDialogue(Dialogue dialogue) 
+
+    public void StartDialogue(Dialogue dialogue)
     {
         Debug.Log("Starting conversation...");
-
         animator.SetBool("isOpen", true);
-
         sentences.Clear();
 
-        Debug.Log("Dialogue has " + dialogue.sentences.Length + " sentences.");
         foreach (string sentence in dialogue.sentences)
         {
-            Debug.Log("Adding sentence: " + sentence);
             sentences.Enqueue(sentence);
         }
 
         DisplayNextSentence();
     }
+
+    public void OnContinueButtonPressed()
+    {
+        if (isTyping)
+        {
+            // First click
+            StopAllCoroutines();
+            dialogueText.text = currentSentence;
+            isTyping = false;
+        } else 
+        {
+            DisplayNextSentence();
+        }
+    }
+
     public void DisplayNextSentence()
     {
-        Debug.Log("Sentences remaining: " + sentences.Count);
         if (sentences.Count == 0)
         {
             EndDialogue();
             return;
         }
         string sentence = sentences.Dequeue();
-        Debug.Log("Displaying sentence: " + sentence);
-        Debug.Log("Sentences left after dequeue: " + sentences.Count);
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
     }
 
     IEnumerator TypeSentence(string sentence)
     {
+        currentSentence = sentence;
+        isTyping = true;
         dialogueText.text = "";
+
         foreach (char letter in sentence.ToCharArray())
         {
             dialogueText.text += letter;
             yield return new WaitForSeconds(0.05f);
         }
+
+        isTyping = false;
     }
-    
+
     void EndDialogue()
-    {        Debug.Log("End of conversation.");
+    {
+        Debug.Log("End of conversation.");
         dialogueText.text = "Meow";
         animator.SetBool("isOpen", false);
     }
--- Assets/Scripts/PickupScene/ClickToHide.cs
+++ Assets/Scripts/PickupScene/ClickToHide.cs
@@ -3,18 +3,38 @@ using UnityEngine.InputSystem;
 
 public class ClickToHide : MonoBehaviour
 {
+    public Transform player;
+    public Transform npc;
+    public float detectionRadius = 3f;
+    private bool isPlayerNearby = false;
+
     void Update()
     {
-        if (Mouse.current == null || !Mouse.current.leftButton.wasPressedThisFrame)
-            return;
+        // check distance between player and npc
+        float distance = Vector3.Distance(player.position, npc.position);
+        isPlayerNearby = distance <= detectionRadius;
 
-        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        // mouse click
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
         {
-            if (hit.transform == transform && hit.transform.GetComponent<Outline>().enabled == true)
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                hit.transform.GetComponent<DialogueTrigger>().TriggerDialogue();
+                if (hit.transform == transform && hit.transform.GetComponent<Outline>().enabled == true)
+                {
+                    hit.transform.GetComponent<DialogueTrigger>().TriggerDialogue();
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+
+        if (Keyboard.current != null && Keyboard.current.eKey.wasPressedThisFrame && isPlayerNearby) 
+        {
+            Outline outline = GetComponent<Outline>();
+            if (outline != null && outline.enabled)
+            {
+                GetComponent<DialogueTrigger>()?.TriggerDialogue();
                 gameObject.SetActive(false);
             }
         }
